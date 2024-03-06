43fc71fde349eee47df6a0a5716de5f8b1e069f3
NicolasEduardoDiaz <160768951+NicolasEduardoDiaz@users.noreply.github.com>
2024-03-06 07:24:36 -0500

    pasar de centimetros a pulgadas

---
+++ Ejercicio_4.py
@@ -0,0 +1,5 @@
+#Escriba un programa que convierta de centímetros a pulgadas. Una pulgada es igual a 2.54 centímetros.
+print("Hola usuario, soy un programa para pasar de centrimetros a pulgadas")
+centimetros = int(input("Ingresa los centimetros "))
+pulgadas = centimetros / 2.54
+print(centimetros, "cm = ", pulgadas,"in")
