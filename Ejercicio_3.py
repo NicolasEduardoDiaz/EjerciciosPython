470cdbc5bc0f42d7a497bf36aca7df458cc26351
NicolasEduardoDiaz <160768951+NicolasEduardoDiaz@users.noreply.github.com>
2024-03-06 07:19:43 -0500

    Promedio de 4 notas

---
+++ Ejercicio_3.py
@@ -0,0 +1,12 @@
+#Escriba un programa que calcule el promedio de 4 notas ingresadas por el usuario:
+print("Hola usuario, soy un programa para calcular promedio de 4 notas")
+print("Ingresa 4 notas ")
+
+not1 = int(input("Nota 1 "))
+not2 = int(input("Nota 2 "))
+not3 = int(input("Nota 3 "))
+not4 = int(input("Nota 4 "))
+
+promedio = (not1 + not2 + not3 + not4)/4
+
+print("El promedio de las cuatro notas es ", promedio)
