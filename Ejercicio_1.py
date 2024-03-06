e657e67de4ee79d3235ef010cc062dfdaeb07b62
NicolasEduardoDiaz <160768951+NicolasEduardoDiaz@users.noreply.github.com>
2024-03-06 07:06:18 -0500

    Porgrama para leer el nombre

---
+++ Ejercicio_1.py
@@ -0,0 +1,4 @@
+#Escriba un programa que pida al usuario que escriba su nombre, y lo salude llamándolo por su nombre.
+print("Hola usuario, soy un programa para escribir el nombre el cual me diga")
+nombre= input("ingrese su nombre ")
+print("Hola ", nombre, " un gusto")
