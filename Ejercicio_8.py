e9ec786891919655044fd500f118ca1ebdb4d1a0
NicolasEduardoDiaz <160768951+NicolasEduardoDiaz@users.noreply.github.com>
2024-03-06 08:45:07 -0500

    nuemro decimal

---
+++ Ejercicio_8.py
@@ -0,0 +1,6 @@
+#Escriba un programa que entregue la parte decimal de un número real ingresado por el usuario.
+print("hola usuario, soy un programa para leer la parte decimal de un numero")
+print("por favor digita el numero decimal ")
+numero = float(input("ingresa el numero decimal: "))
+decimal = numero - (int(numero))
+print("el numeor decimal de ",numero, "es ",decimal)
